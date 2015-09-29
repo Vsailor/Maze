c567c18a3774b775168203f01ad94888e46d5271
Ivan <sirius9764@gmail.com>
2015-09-29 23:10:28 +0300

    Init

---
+++ Assets/Login.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Login : Photon.MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Test room", new RoomOptions(), TypedLobby.Default);
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Cube", Vector3.up * 5, Quaternion.identity, 0);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Photon.MonoBehaviour {
+    CharacterController controller;
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (photonView.isMine)
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            if (v != 0)
+            {
+                controller.Move(transform.forward * v * 5f * Time.deltaTime);
+            }
+            if (h != 0)
+            {
+                transform.Rotate(Vector3.up * h);
+            }
+            controller.Move(Physics.gravity * Time.deltaTime);
+        }
+	}
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        Vector3 pos = transform.position;
+        Quaternion rot = transform.rotation;
+        stream.Serialize(ref pos);
+        stream.Serialize(ref rot);
+        if (stream.isReading)
+        {
+            transform.position = pos;
+            transform.rotation = rot;
+        }
+    }
+}
